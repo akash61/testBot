a1ccee0c462f085e8984573c3466c4ea458c4705
Akash Sisodia <akash@akashs-mac-mini.local>
2018-05-12 15:37:25 +0530

    Initial Commit

---
+++ BotTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BotTest
+//
+//  Created by Akash Singh Sisodia on 12/05/18.
+//  Copyright © 2018 Neuron Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
